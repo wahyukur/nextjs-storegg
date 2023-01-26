549eda6459b623c6998e3adecd6dc1a8b15d78b9
wahyukur <wahyukur81@gmail.com>
2023-01-26 23:29:53 +0700

    progress

--- components/organisms/SignInForm/index.tsx
+++ components/organisms/SignInForm/index.tsx
@@ -19,7 +19,7 @@ export default function SignInForm() {
         </div>
         <div className="button-group d-flex flex-column mx-auto pt-50">
             <a className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-                href="../index.html" role="button">Continue to Sign In</a>
+                href="/member" role="button">Continue to Sign In</a>
             
             <a className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
                 href="/sign-up" role="button">Sign
